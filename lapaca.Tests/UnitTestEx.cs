e6cffe955732b63302971da06b38cd8332fc82ec
Aitor Fornies <aitgelion@gmail.com>
2021-11-14 17:28:43 +0100

    Allow extra configuration mode with SSL cert check skipping and url compression (#2)

--- lapaca.Tests/UnitTestEx.cs
+++ lapaca.Tests/UnitTestEx.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace lapaca.Tests
 {
-    public class UnitTest
+    public class UnitTestEx
     {
         [Fact]
         public async Task CheckSignature()
@@ -15,7 +15,7 @@ namespace lapaca.Tests
             var url = "http://url.com/tokengrandote";
             var scheme = "{\"title\": ${{obj.title}}}";
 
-            var response = await client.PostAsJsonAsync("/api/wh", new WebHookConfig("m8f6uFJ95yHRvyVDjYESPw==", url, scheme));
+            var response = await client.PostAsJsonAsync("/api/whex", new WebHookConfig("m8f6uFJ95yHRvyVDjYESPw==", url, scheme));
             var finalUrl = await response.Content.ReadFromJsonAsync<string>();
 
             var webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { title = "lapaca" } });
@@ -31,7 +31,7 @@ namespace lapaca.Tests
             var url = "http://url.com/tokengrandote";
             var scheme = "{\"title\": ${{obj.title}}}";
 
-            var response = await client.PostAsJsonAsync("/api/wh", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
+            var response = await client.PostAsJsonAsync("/api/whex", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
             var finalUrl = await response.Content.ReadFromJsonAsync<string>();
 
             var webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { title = "lapaca" } });
@@ -49,7 +49,7 @@ namespace lapaca.Tests
             var url = "http://url.com/tokengrandote";
             var scheme = "{\"title\": ${{obj.title??12}}}";
 
-            var response = await client.PostAsJsonAsync("/api/wh", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
+            var response = await client.PostAsJsonAsync("/api/whex", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
             var finalUrl = await response.Content.ReadFromJsonAsync<string>();
 
             var webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { subTitle = "lapaca" } });
@@ -67,7 +67,7 @@ namespace lapaca.Tests
             var url = "http://url.com/tokengrandote";
             var scheme = "{\"title\": ${{obj.title??\"No title!\"}}}";
 
-            var response = await client.PostAsJsonAsync("/api/wh", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
+            var response = await client.PostAsJsonAsync("/api/whex", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
             var finalUrl = await response.Content.ReadFromJsonAsync<string>();
 
             var webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { subTitle = "lapaca" } });
@@ -85,13 +85,40 @@ namespace lapaca.Tests
             var url = "http://url.com/tokengrandote";
             var scheme = "{\"title\": ${{obj.title??\\{\"name\": \"title\"\\}}}}";
 
-            var response = await client.PostAsJsonAsync("/api/wh", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
+            var response = await client.PostAsJsonAsync("/api/whex", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
             var finalUrl = await response.Content.ReadFromJsonAsync<string>();
 
             var webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { subTitle = "lapaca" } });
             var webHookContent = await webHookResult.Content.ReadAsStringAsync();
 
             Assert.Equal("{\"title\": {\"name\": \"title\"}}", webHookContent);
+        }
+
+        [Fact]
+        public async Task CompareBigUrls()
+        {
+            await using var application = new LapacaApplicationFixture();
+            var client = application.CreateClient();
+
+            var url = "http://url.com/tokengrandote";
+            var scheme = "{\"type\":\"AdaptiveCard\",\"version\":\"1.0\",\"body\":[{\"type\":\"TextBlock\",\"text\": ${{obj.msg}}},{\"type\":\"Image\",\"url\":\"http://adaptivecards.io/content/cats/1.png\"}]}";
+
+            var response = await client.PostAsJsonAsync("/api/wh", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
+            var finalUrl = await response.Content.ReadFromJsonAsync<string>();
+
+            response = await client.PostAsJsonAsync("/api/whex", new WebHookConfig("GuPrRON7FlSloWkUy1oDfQ==", url, scheme));
+            var finalUrlEx = await response.Content.ReadFromJsonAsync<string>();
+
+            // Compare results:
+            var webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { msg = "lapaca" } });
+            var webHookContent = await webHookResult.Content.ReadAsStringAsync();
+
+            webHookResult = await client.PostAsJsonAsync(finalUrl, new { obj = new { msg = "lapaca" } });
+            var webHookContentEx = await webHookResult.Content.ReadAsStringAsync();
+
+            Assert.Equal(webHookContent, webHookContentEx);
+
+            Assert.True(finalUrl?.Length > finalUrlEx?.Length);
         }
     }
 }
--- lapaca/Program.cs
+++ lapaca/Program.cs
@@ -1,3 +1,4 @@
+using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
@@ -9,18 +10,50 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Http clients:
 builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("NoCert")
+    .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback = (httpRequestMessage, cert, cetChain, policyErrors) => true
+    });
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Configuration["enableOpenApi"]?.ToLower()=="true")
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.MapPost("/wh/{b64Signature}/{b64Conf}", async (string b64Signature, string b64Conf,
+    JsonDocument payload, IHttpClientFactory httpClientFactory) =>
+{
+    // Check signature
+    var signature = SafeUrlBase64DecodeData(b64Signature);
+    var hmac = GetHmacData(app.Configuration["hmacKey"], b64Conf);
+    if (!signature.SequenceEqual(hmac))
+    {
+        return Results.Unauthorized();
+    }
+
+    // Get WebHook Configuration
+    WebHookConfig? config;
+    using (var deflated = new MemoryStream(b64Conf.Length))
+    using (var inflater = new DeflateStream(new MemoryStream(SafeUrlBase64DecodeData(b64Conf)), CompressionMode.Decompress))
+    {
+        inflater.CopyTo(deflated);
+        deflated.Seek(0, SeekOrigin.Begin);
+
+        config = ProtoBuf.Serializer.Deserialize<WebHookConfig>(deflated);
+    }
+
+    return await ProcessWebHook(httpClientFactory, config, payload);
+}).WithName("WebHookExCompressed");
 
 app.MapPost("/wh/{b64Signature}/{b64url}/{b64scheme}", async (string b64Signature, string b64url, string b64scheme,
     JsonDocument payload, IHttpClientFactory httpClientFactory) =>
@@ -35,45 +68,45 @@ app.MapPost("/wh/{b64Signature}/{b64url}/{b64scheme}", async (string b64Signatur
 
     var url = SafeUrlBase64Decode(b64url);
     var scheme = SafeUrlBase64Decode(b64scheme);
+    
+    WebHookConfig config = new WebHookConfig("", url, scheme);
 
-    // Replace
-    Regex regex = new Regex(@"\$\{\{(.*?(?:\\\})*?)\}\}");
-    // \$\{\{(.*?(?:\})*)\}\} matchea bien pero en bloque
-    var replacements = regex.Matches(scheme).Reverse();
-    foreach (Match replacement in replacements)
-    {
-        var components = replacement.Groups[1].Value.Split("??", 2);
-
-        var value = GetValue(payload, components[0]) ?? (components.Length == 2 ? components[1].Replace("\\{","{").Replace("\\}","}"): @"null");
-        scheme = scheme.Replace(replacement.Groups[0].Value, value);
-    }
-
-    // Call the final Endpoint
-    var httpClient = httpClientFactory.CreateClient();
-    var post = await httpClient.PostAsync(url, new StringContent(scheme, Encoding.UTF8, "application/json"));
-    if (!post.IsSuccessStatusCode)
-    {
-        return Results.Problem();
-    }
-
-    return Results.Stream(await post.Content.ReadAsStreamAsync(), "application/json");
-
-    //return Results.Ok();
+    return await ProcessWebHook(httpClientFactory, config, payload);
 }).WithName("webhook");
 
-app.MapPost("/api/wh", async (CreateUrlDto createUrlDto) =>
+// API
+app.MapPost("/api/wh", (WebHookConfig createUrlDto) =>
 {
     // Data
-    var url = SafeUrlBase64Encode(createUrlDto.url);
-    var scheme = SafeUrlBase64Encode(createUrlDto.scheme);
+    var url = SafeUrlBase64Encode(createUrlDto.Url);
+    var scheme = SafeUrlBase64Encode(createUrlDto.Scheme);
 
     // Generate signature
-    var hmac = GetHmac(createUrlDto.hmacKey, url, scheme);
+    var hmac = GetHmac(createUrlDto.Hmac, url, scheme);
     var signature = SafeUrlBase64EncodeData(hmac);
-
+    
+    // Simple URL
     var finalUrl = $"/wh/{signature}/{url}/{scheme}";
+
     return Results.Ok(finalUrl);
 }).WithName("createWebHook");
+
+app.MapPost("/api/whex", (WebHookConfig createUrlDto) =>
+{
+    using (var deflated = new MemoryStream())
+    {
+        using (var compressor = new DeflateStream(deflated, CompressionMode.Compress, true))
+        {
+            ProtoBuf.Serializer.Serialize(compressor, createUrlDto);
+        }
+
+        var conf = SafeUrlBase64EncodeData(deflated.ToArray());
+        var signature = SafeUrlBase64EncodeData(GetHmacData(createUrlDto.Hmac, conf));
+
+        var finalUrl = $"/wh/{signature}/{conf}";
+        return Results.Ok(finalUrl);
+    }
+}).WithName("createWebHookEx");
 
 
 app.Run();
@@ -96,6 +129,34 @@ string? GetValue(JsonDocument json, string path)
     return element.GetRawText();
 }
 
+async Task<IResult> ProcessWebHook(IHttpClientFactory httpClientFactory, WebHookConfig config, JsonDocument payload)
+{
+    // Replace
+    string finalPayload = config.Scheme;
+    Regex regex = new Regex(@"\$\{\{(.*?(?:\\\})*?)\}\}");
+    var replacements = regex.Matches(config.Scheme).Reverse();
+    foreach (Match replacement in replacements)
+    {
+        var components = replacement.Groups[1].Value.Split("??", 2);
+
+        var value = GetValue(payload, components[0]) ?? (components.Length == 2 ? components[1].Replace("\\{", "{").Replace("\\}", "}") : @"null");
+        finalPayload = finalPayload.Replace(replacement.Groups[0].Value, value);
+    }
+
+    // Call the final endpoint
+    var httpClient = config.SkipCertVerification ?
+        httpClientFactory.CreateClient("NoCert") :
+        httpClientFactory.CreateClient();
+
+    var post = await httpClient.PostAsync(config.Url, new StringContent(finalPayload, Encoding.UTF8, "application/json"));
+    if (!post.IsSuccessStatusCode)
+    {
+        return Results.Problem();
+    }
+
+    return Results.Stream(await post.Content.ReadAsStreamAsync(), "application/json");
+}
+
 string SafeUrlBase64EncodeData(byte[] data)
   => Convert.ToBase64String(data).Replace("+", "-").Replace("/", "_");
 
@@ -109,9 +170,17 @@ string SafeUrlBase64Decode(string str)
   => Encoding.UTF8.GetString(SafeUrlBase64DecodeData(str));
 
 byte[] GetHmac(string hmacKey, string b64Url, string b64Scheme)
-    => HMACSHA256.HashData(SafeUrlBase64DecodeData(hmacKey), Encoding.UTF8.GetBytes(b64Url + b64Scheme));
+    => GetHmacData(hmacKey, b64Url + b64Scheme);
 
-public record CreateUrlDto(string hmacKey, string url, string scheme);
+byte[] GetHmacData(string hmacKey, string b64Data)
+    => HMACSHA256.HashData(SafeUrlBase64DecodeData(hmacKey), Encoding.UTF8.GetBytes(b64Data));
+
+public record WebHookConfig(
+    [property: ProtoBuf.ProtoIgnore]    string Hmac,
+    [property: ProtoBuf.ProtoMember(1)] string Url,
+    [property: ProtoBuf.ProtoMember(2)] string Scheme,
+    [property: ProtoBuf.ProtoMember(3)] bool SkipCertVerification = false
+);
 
 // Make the implicit Program class public so test projects can access it
 public partial class Program { }
